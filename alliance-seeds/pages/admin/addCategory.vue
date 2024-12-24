36648a8f33a8bc9365394c9da55d2b0e3947e2e8
Justin Smith <justin.smith@accutrak.co.za>
2024-12-24 15:06:08 +0200

    Add Category page working
    - add category page working
    - router breaks when navigating back to products page

--- alliance-seeds/pages/admin/addCategory.vue
+++ alliance-seeds/pages/admin/addCategory.vue
@@ -1,17 +1,17 @@
 <template>
-    <br>
-  <div >
-    <div >
+  <br />
+  <div>
+    <div>
       <div class="col-xl-9 col-lg-10 col-md-12 col-sm-12 mx-auto">
-        <div class="tm-bg-primary-dark tm-block tm-block-h-auto ">
-          <div  >
+        <div class="tm-bg-primary-dark tm-block tm-block-h-auto">
+          <div>
             <div class="col-12 d-flex justify-content-center">
               <h2 class="tm-block-title d-inline-block">Add Category</h2>
             </div>
           </div>
-          <div class="row tm-edit-product-row d-flex justify-content-center" >
+          <div class="row tm-edit-product-row d-flex justify-content-center">
             <div class="col-xl-6 col-lg-6 col-md-12">
-              <form action="" class="tm-edit-product-form">
+              <form @submit.prevent="addCategory" class="tm-edit-product-form">
                 <div class="form-group mb-3">
                   <label for="name">Category Name</label>
                   <input
@@ -19,25 +19,47 @@
                     name="name"
                     type="text"
                     class="form-control validate"
+                    v-model="categoryName"
                     required
                   />
                 </div>
 
-                <div class=" col-md-12 mx-auto mb-4">
-                  <div class="tm-product-img-dummy mx-auto">
+                <div class="col-md-12 mx-auto mb-4">
+                  <div
+                    class="tm-product-img-dummy mx-auto"
+                    v-if="!categoryImage"
+                    ref="dummyImage"
+                  >
                     <i
                       class="fas fa-cloud-upload-alt tm-upload-icon"
                       onclick="document.getElementById('fileInput').click();"
                     ></i>
                   </div>
+                  <div class="tm-product-img-dummy mx-auto" v-else>
+                    <img
+                      :src="categoryImage"
+                      alt="Selected Image"
+                      class="img-fluid"
+                      :style="{ height: dummyImageHeight + 'px' }"
+                    />
+                  </div>
                   <div class="custom-file mt-3 mb-3">
-                    <input id="fileInput" type="file" style="display: none" />
                     <input
+                      id="fileInput"
+                      type="file"
+                      style="display: none"
+                      @change="onFileChange"
+                    />
+                    <!-- <input
                       type="button"
                       class="btn btn-primary btn-block mx-auto"
                       value="UPLOAD CATEGORY IMAGE"
                       onclick="document.getElementById('fileInput').click();"
-                    />
+                    /> -->
+                    <!-- <v-btn onclick="document.getElementById('fileInput').click();"> Delete Previous image </v-btn>
+                    <br>
+                    <br> -->
+                    <v-btn onclick="document.getElementById('fileInput').click();"> UPLOAD CATEGORY IMAGE  </v-btn>
                   </div>
                 </div>
                 <div class="col-12">
@@ -60,9 +82,10 @@
 <script>
 import navbar from "~/components/admin/navbar.vue";
 import useSessionStore from "~/stores/session";
-// import { useHead } from '@vueuse/head';
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
+import Product from "@/services/server/product";
+
 
 export default {
   name: "IndexPage",
@@ -72,12 +95,47 @@ export default {
   data() {
     return {
       session: null,
+      categoryName: "",
+      categoryImage: null,
+      dummyImageHeight: 0,
     };
   },
   mounted() {
     this.session = useSessionStore();
+    this.dummyImageHeight = this.$refs.dummyImage.clientHeight;
   },
-  methods: {},
+  methods: {
+    onFileChange(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.categoryImage = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    },
+    async addCategory() {
+      console.log("adding category");
+      console.log("Category Name: " + this.categoryName);
+      console.log("Category Image: " + this.categoryImage);
+
+      let data = {
+        name: this.categoryName,
+        photo: this.categoryImage,
+      }
+
+      let response = await Product.addCtegory(data);
+
+      if(response != null && response.meta.success){
+        toast.success("Category Added", { autoClose: 3000, hideProgressBar: true });
+        this.$router.back();
+        return;
+      }
+      toast.error("There was an error adding category", { autoClose: 3000, hideProgressBar: true });
+
+    },
+  },
 };
 </script>
 
@@ -85,5 +143,16 @@ export default {
 #footer {
   position: fixed;
   bottom: 0;
+}
+.cat-image {
+  height: 300px; /* Set your desired height here */
+  width: auto;
+  object-fit: contain;
+}
+
+.img-thumbnail {
+  max-width: 100%;
+  max-height: 300px; /* Adjust this value as needed */
+  object-fit: contain;
 }
 </style>
--- alliance-seeds/services/server/product.js
+++ alliance-seeds/services/server/product.js
@@ -5,18 +5,48 @@ class Product extends ServerBase {
     const endpoint = "/client/getAllProducts";
     const response = await this.getRequest(endpoint);
     return response;
+
+        // response = [
+    //   {
+    //     prodCode: "string",
+    //     category: "string", 
+    //     name: "string",
+    //     document: "base64 string",
+    //     info: "json string",
+    //     quantity: int,
+    //   },
+    // ]
   }
 
   static async getCategoryNames() {
     const endpoint = "/client/getCategoryNames";
     const response = await this.getRequest(endpoint);
     return response;
+    // response = ["cat1", "cat2"]
   }
 
   static async getProductsByCategory(category){
     const endpoint = '/client/getProductsByCategory';
     const params = { category: category };
     const response = await this.getRequest(endpoint, params); 
+    return response;
+
+    // response = [
+    //   {
+    //     prodCode: "string",
+    //     category: "string", 
+    //     name: "string",
+    //     document: "base64 string",
+    //     info: "json string",
+    //     quantity: int,
+    //   },
+    // ]
+  }
+
+  static async addCtegory(data){
+    // data = {name: "string", photo: "base64 string"}
+    const endpoint = '/client/addCategory';
+    const response = await this.postRequest(endpoint, data); 
     return response;
   }
 
